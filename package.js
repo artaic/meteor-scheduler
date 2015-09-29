ba6c8daf7f3bd49b15c2ece283385050dec8639b
Alex Frazer <alexadfrazer@gmail.com>
2015-09-29 10:13:49 -0400

    bump version

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
   name: 'insightfil:scheduler',
-  version: '0.0.7',
+  version: '0.0.8',
   summary: 'Manage batches of jobs',
   git: '',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2.0.2');
 
   api.use([
     'mongo',
     'underscore',
-    'grigio:babel@0.1.7'
+    'ecmascript'
   ]);
 
   api.export('Scheduler');
-  api.addFiles('scheduler.es6');
+  api.addFiles('scheduler.js');
 });
 
 Package.onTest(function(api) {
